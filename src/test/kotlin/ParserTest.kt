271fb4cfa5d2c27aca660143d546314270307736
Kareez <m.h.shams@gmail.com>
2019-04-01 22:30:27 +0200

    basc working version

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -4,7 +4,15 @@ import org.junit.Test
 class ParserTest {
 
     @Test
-    fun `it works`() {
-        assertThat(true).isFalse()
+    fun `Char Parser works as expected`() {
+
+        assertThat(run(pChar('A'), State("ZBC")))
+            .isEqualTo(Failure<Char>("Expected A. Got Z"))
+
+        assertThat(run(pChar('A'), State("ABC")))
+            .isEqualTo(Success('A', State(input = "ABC", col = 1, pos = 1)))
+
+        assertThat(run(pChar('B'), State(input = "ABC", col = 1, pos = 1)))
+            .isEqualTo(Success('B', State(input = "ABC", col = 2, pos = 2)))
     }
 }
